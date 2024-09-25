959128c81228fdc5f9104fb4fcc5f97b924bd2be
Antoine <antoine.gay-puig@univ-tlse3.fr>
2024-09-25 10:42:51 +0200

    Creation des premieres classes et ajout du super user Admin

--- crayon/crayon/settings.py
+++ crayon/crayon/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "django.contrib.admin",
-    "django.contrib.high_level",
+    "high_level",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+# crayon/models.py
+
+class Ville(models.Model):
+    nom = models.CharField(max_length=200)
+    code_postal = models.IntegerField(default=31400)
+    prixm2 = models.IntegerField(default=400)
+    
+class Local(models.Model):
+    nom = models.CharField(max_length=200)
+    ville = models.ForeignKey(
+            Ville,
+            on_delete=models.PROTECT,
+            )
+    surface = models.CharField(max_length=200)
+
+class Siege(Local):
+    pass # pour dire a python que ce n'est pas une erreur et que l'on ne met rien dedans
+   
+class Machine(models.Model):
+    nom = models.CharField(max_length=200)
+    prix = models.IntegerField(default=1)
+    n_serie = models.CharField(max_length=200,default='A') 
+   
+class Usine(Local):
+      machine = models.ForeignKey(
+            Machine,
+            on_delete=models.PROTECT,
+            )
+
