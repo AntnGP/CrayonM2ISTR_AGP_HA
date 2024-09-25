e8c206d5e97cb4344a7e85b3ceeefedad242db14
Antoine <antoine.gay-puig@univ-tlse3.fr>
2024-09-25 10:07:33 +0200

    ajout du high level dans les roles utilisateurs de django

--- crayon/crayon/settings.py
+++ crayon/crayon/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "django.contrib.admin",
+    "django.contrib.high_level",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
