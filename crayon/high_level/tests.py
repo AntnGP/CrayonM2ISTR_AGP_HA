aa784b2f08191aaa868d41c537a11a8d9aea8547
Antoine <antoine.gay-puig@univ-tlse3.fr>
2024-10-01 11:54:50 +0200

    fin seance, tests pas fini a finir pas fonctionnel

--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -9,3 +9,14 @@ from .models import Machine
         Machine.objects.create(nom="scie", prix=1000, n_serie="1683AI2")
         self.assertEqual(Machine.objects.count(), 2)"""
 
+    class CostTest(Testcase):
+        def test_cost(self):
+            V1 = Ville.objects.create(nom="scie", code_potal=31670, prixm2 = 2000)
+            U1 = V1.usine_set.get(nom="BurgerKing",surface=50)
+            M1 = U1.machine_set.get(nom="scie", prix=1000, n_serie="1683AI2")
+            M2 = U1.machine_set.get(nom="tasla", prix=2000, n_serie="HPelite720")
+            
+            R1 = Ressource.object.create
+            S1 = Stock.objects.create
+            
+            
