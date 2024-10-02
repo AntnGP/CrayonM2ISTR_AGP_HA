30f71354fb85d95e6bba2ca052a91399fad96efd
Antoine <antoine.gay-puig@univ-tlse3.fr>
2024-10-02 16:11:03 +0200

    fonctionement de cost()

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -19,7 +19,7 @@ class Local(models.Model):  # Represente un batiment situé dans une ville
         Ville,
         on_delete=models.PROTECT,
     )
-    surface = models.CharField(max_length=200)
+    surface = models.IntegerField(default=200)
 
     def __str__(self):
         return f"local {self.nom} situé a {self.ville.nom},{self.ville.code_postal} d'une sruface de {self.surface}"
@@ -35,6 +35,9 @@ class Machine(models.Model):  # Represente une machine situé dans une usine
     prix = models.IntegerField(default=1)
     n_serie = models.CharField(max_length=200, default="A")
 
+    def cost(self):
+        return self.prix
+
     def __str__(self):
         return f"Machine {self.nom},{self.n_serie}"
 
@@ -44,102 +47,108 @@ class Usine(Local):  # Represente un type de batiment specifique
         Machine,
         # on_delete=models.PROTECT,  #pas supporté par ManytoMany
     )
-    def costs(self):
+
+    def cost(self):
         cout = 0
-        for m in self.machines.all(): #pour toutes les machines (m) de l'uine on recupere le prix par la methode cost et on l'aditionne au prix du terrain
+        for m in self.machines.all():  # pour toutes les machines (m) de l'uine on recupere le prix par la methode cost et on l'aditionne au prix du terrain
             cout = cout + m.cost()
-        for s in self.stock_set.all(): #stock_set est un paramtre generé par l'intepreteur qui liste tous les stocks de l'usine au quel on accede 
+        for s in self.stock_set.all():  # stock_set est un paramtre generé par l'intepreteur qui liste tous les stocks de l'usine au quel on accede
             cout = cout + s.cost()
-        cout = self.ville.prixm2 * self.surface + cout
+        cout = int(self.ville.prixm2) * int(self.surface) + int(cout)
         f"Coute {cout}€"
         return cout
-        
+
     def __str__(self):
         return f"Usine {self.nom} de {self.ville.nom},{self.ville.code_postal} d'une sruface de {self.surface}m2"
 
 
-
 class Objet(models.Model):  # Represente un objet
-    nom = models.CharField(max_length=200,default="truc")
+    nom = models.CharField(max_length=200, default="truc")
     prix = models.IntegerField(default=400)
 
     def __str__(self):
         return f"{self.nom},{self.prix}"
-    
-class Ressource(Objet):  # Represente un type d'objet
 
-    def acheter(quantite):
-        prix = {self.prix}*quantite
+
+class Ressource(Objet):  # Represente un type d'objet
+    def acheter(self):  # achete une ressource
+        prix = {self.prix}
         f"Prix d'achat = {prix}€"
         return prix
-
 
     def __str__(self):
         return f"Ressource :{self.nom},{self.prix} "
 
 
 class Stock(models.Model):  # Represente un type de batiment specifique
-    ressource = models.ManyToManyField(Ressource,)
+    ressource = models.ForeignKey(
+        Ressource,
+        on_delete=models.PROTECT,
+    )
     nombre = models.IntegerField(default=0)
     local = models.ForeignKey(
-                                Local,
-                                on_delete=models.PROTECT,
+        Local,
+        on_delete=models.PROTECT,
     )
-    
-    def costs(self):
+
+    def cost(self):
         cout = self.ressource.prix * self.nombre
         f"Coute {cout}€"
         return cout
-    
+
     def __str__(self):
         return f"Stock situe a {self.local.nom} dans {self.local.ville.nom},{self.local.ville.code_postal} d'une surface de {self.local.surface}"
 
 
 class QuantiteRessource(models.Model):  # Represente un enssemble de ressources
     ressource = models.ForeignKey(
-                                Ressource,
-                                on_delete=models.PROTECT,
+        Ressource,
+        on_delete=models.PROTECT,
     )
     quantite = models.IntegerField(default=0)
-    
-    def costs(self):
+
+    def cost(self):
         cout = self.ressource.prix * self.quantite
         f"Coute {cout}€"
         return cout
-    
+
     def __str__(self):
         return f"Demande {self.quantite} {self.ressource.nom}"
+
 
 class Etape(models.Model):  # Represente une etape de production
     nom = models.CharField(max_length=200)
     duree = models.IntegerField(default=0)
     machine = models.ForeignKey(
-                                Machine,
-                                on_delete=models.PROTECT,
+        Machine,
+        on_delete=models.PROTECT,
     )
-    
+
     ressources = models.ForeignKey(
-                                QuantiteRessource,
-                                on_delete=models.PROTECT,
+        QuantiteRessource,
+        on_delete=models.PROTECT,
     )
     etape_suivante = models.ForeignKey(
-                                'self',
-                                blank=True,
-                                null=True,
-                                on_delete=models.CASCADE,
+        "self",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
-        return f"Etape demandant {self.ressources.ressource.nom} avec {self.machine.nom}"
+        return (
+            f"Etape demandant {self.ressources.ressource.nom} avec {self.machine.nom}"
+        )
+
     # Une etape ne crée pas de produit car c'est trop compliquer
-    
+
 
 class Produit(models.Model):  # Represente un objet que l'on vend
     premiere_etape = models.ForeignKey(
-                                Etape,
-                                on_delete=models.PROTECT,
+        Etape,
+        on_delete=models.PROTECT,
     )
     quantite = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return f"Demande {self.quantite} {self.ressource.nom}"
--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -1,22 +1,30 @@
 from django.test import TestCase
-
-from .models import Machine
-
+from .models import Ville, Stock, Ressource, Usine, Machine
 
 """class MachineModelTests(TestCase):
-   def test_machine_creation(self):
-        self.assertEqual(Machine.objects.count(), 0)
-        Machine.objects.create(nom="scie", prix=1000, n_serie="1683AI2")
-        self.assertEqual(Machine.objects.count(), 2)"""
+def test_machine_creation(self):
+     self.assertEqual(Machine.objects.count(), 0)
+     Machine.objects.create(nom="scie", prix=1000, n_serie="1683AI2")
+     self.assertEqual(Machine.objects.count(), 2)"""
 
-    class CostTest(Testcase):
-        def test_cost(self):
-            V1 = Ville.objects.create(nom="scie", code_potal=31670, prixm2 = 2000)
-            U1 = V1.usine_set.get(nom="BurgerKing",surface=50)
-            M1 = U1.machine_set.get(nom="scie", prix=1000, n_serie="1683AI2")
-            M2 = U1.machine_set.get(nom="tasla", prix=2000, n_serie="HPelite720")
-            
-            R1 = Ressource.object.create
-            S1 = Stock.objects.create
-            
-            
+
+class CostTest(TestCase):
+    def test_cost(self):
+        V1 = Ville.objects.create(nom="scie", code_postal=31670, prixm2=2000)
+
+        U1 = Usine.objects.create(nom="BurgerKing", ville=V1, surface=50)
+
+        M1 = Machine.objects.create(nom="scie", prix=1000, n_serie="1683AI2")
+        U1.machines.add(
+            M1
+        )  # pour du many to many , il faut faire un objet_set puis l'ajouter avec add()
+        M2 = Machine.objects.create(nom="tasla", prix=2000, n_serie="HPelite720")
+        U1.machines.add(M2)
+
+        R1 = Ressource.objects.create(nom="Bois 1kg", prix=10)  # 1000
+        R2 = Ressource.objects.create(nom="Graphite 1m", prix=15)  # 50
+        Stock.objects.create(ressource=R1, nombre=1000, local=U1)
+        Stock.objects.create(ressource=R2, nombre=50, local=U1)
+
+        self.assertEqual(Usine.objects.first().cost(), 110750)
+        f"Valeur = {U1.cost()}"
